a6c4e320db5f5cd15644c417e4cc2dae8849bb0d
bsyso <bsyso@softserveinc.com>
2022-12-30 10:05:11 +0100

    Initial commit

---
+++ src/main/java/com/photo_app_account_service/PhotoAppAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.photo_app_account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/photo_app_account_service/PhotoAppAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo_app_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
